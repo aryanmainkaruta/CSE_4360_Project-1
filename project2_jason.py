9a154d64d3cefffc6278853a5c0a51b8752c26c3
Jaster4000a <jasoncabrejos@hotmail.com>
2024-11-10 19:33:34 -0600

    Add files via upload

--- project2_jason.py
+++ project2_jason.py
@@ -53,32 +53,6 @@ def DriveTrain(correctionValue):
 sweepUP()
 wait(1000)
 SweepMotor.brake()
-
-
-# def sweepForWall():
-
-#     closest_distance = float('inf')  # Start with a very large distance
-#     closest_angle = 0  # To store the angle where the closest wall was detected
-#     initial_angle = gyro.angle()  # Get the initial angle
-
-#     # Begin 360-degree sweep
-#     while abs(gyro.angle() - initial_angle) < 360:
-#         SweepMotor.run(50)  # Run the motor to turn the robot
-
-#         # Check distance with ultrasonic sensor
-#         current_distance = ultra.distance()
-
-#         # If the current distance is less than the closest found so far, update closest
-#         if current_distance < closest_distance:
-#             closest_distance = current_distance
-#             closest_angle = gyro.angle()
-
-#     # Stop turning after completing the 360-degree sweep
-#     SweepMotor.brake()
-
-#     # Turn to the angle where the closest wall was detected
-#     TurnForAngle(closest_angle - gyro.angle())
-
 while True:
     DriveTrain(ultra.distance()-wallHuggingDistance)
     if(button.pressed()):
@@ -88,25 +62,7 @@ while True:
         LeftMotor.run(-50)
         wait(1000)
         TurnForAngle(90)
-        
-        
-# def extinguish_fire():        
-#     print("Activating fan to extinguish fire...")
-#     # Run fan motor for a set duration
-#     fan_motor.run(1000)  # Run fan at full speed
-#     wait(3000)  # Run fan for 3 seconds
-#     fan_motor.stop()  # Stop the fan after extinguishing
-#     print("Fire extinguished.")
 
-# def detect_fire():
-#     # Scan for the color of the fire (simulated by detecting a specific color or intensity)
-#     if color_sensor.color() == Color.RED:  # Assuming red paper represents fire
-#         print("Fire detected!")
-#         # Raise an alarm (could be visual or audible)
-#         hub.speaker.beep()  # Beep to indicate fire detection
-#         return True
-#     else:
-#         return False
 
 
 
